5ddd0b349f024459720a4d90c19f1ec87d17f457
EkremAkkaya <121679843+bartzabeelzebub@users.noreply.github.com>
2023-03-25 22:30:26 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace StarbucksAndNero
         static void Main(string[] args)
         {
             BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
-            customerManager.Save(new Customer {DateOfBirth = new DateTime(2004, 3, 28), FirstName = "Baran", LastName = "Gülaçar", NationalityId= "13262382692"});
+            customerManager.Save(new Customer {DateOfBirth = new DateTime(2004, 3, 28), FirstName = "John", LastName = "Doe", NationalityId= "12345678901"});
             Console.ReadLine();
         }
     }
